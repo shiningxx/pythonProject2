054b697032396232be602c7a8e7721538af56562
Roshni <roshnipasi7@gmail.com>
2024-06-21 11:50:15 +0530

    palindrome

---
+++ PRACTISE/palindrome.py
@@ -0,0 +1,11 @@
+p= input("enter the string")
+#k=reverse(p)
+k=p[::-1]
+#print(k)
+if p==k:
+    print("entered string is palindrome")
+else:
+    print("entered string is not palindrome")
+
+
+#reversed function can also be used
