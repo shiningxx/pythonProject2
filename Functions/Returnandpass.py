2e11058312481d07b7f0e52df6b192fb167558b3
Roshni <roshnipasi7@gmail.com>
2024-07-04 23:34:07 +0530

    returnandpass

---
+++ Functions/Returnandpass.py
@@ -0,0 +1,15 @@
+# def future_function():
+#     pass
+#
+# # this will execute without any action or error
+# future_function()
+
+
+def find_square(num):
+    result = num * num
+    return result
+
+# function call
+square = find_square(3)
+
+print('Square:', square)
