d09632785f7197d99534b48a6ac3746189108fdc
Roshni <roshnipasi7@gmail.com>
2024-07-04 23:35:59 +0530

    vowelsbyfunction.py

---
+++ examples/vowelsbyfunction.py
@@ -0,0 +1,22 @@
+# str = input("enter the string")
+# def vow(str):
+#     count=0
+#     j=0
+#     for i in str:
+#         if i in ('a','e','i','o','u'):
+#             count=count+1
+#         else:
+#             j=j+1
+#     print(count,j)
+
+
+str = input("enter the string")
+def vow(str):
+    count, j = 0, 0
+    for i in str:
+        if i in ('a', 'e', 'i', 'o', 'u'):
+            count = count + 1
+        else:
+            j = j + 1
+    return count, j
+print(vow(str))
